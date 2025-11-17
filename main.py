e01102685fff40026fa5580c807a92cda8763418
LKS <lingasamy.kandasamy@gmail.com>
2025-11-17 18:44:24 +0530

    rich implementation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import practices
-
+from rich import print
+from rich.console import Console
+from rich.table import Table
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -14,13 +16,21 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    console = Console()
+    table = Table(title="Results Table")
+    table.add_column("Sl. no.", justify="center", style="cyan")
+    table.add_column("Function", justify="left", style="magenta")
+    table.add_column("Result", style="green")    
     print_hi('Namashivaya Narayana Swami Narayan ')
-    print(practices.fibonacci(10))
-    print(practices.is_palindrome("malayalam"))
+    table.add_row("1", "fibonacci", str(practices.fibonacci(10)))
+    table.add_row("2", "is_palindrome", str(practices.is_palindrome("malayalam")))
     ls = ["aabccddd","aaabccddd","aaabccddd","baab","aa","ab","aabbccddeeff","abcdeffedcba","aaabccba"]
     #print(set(s))
-    for s in ls:
+    for index, s in enumerate(ls, start=3):
         result = practices.superReducedString(s)
-        print(result)    
+        table.add_row(str(index), "superReducedString", str(result))
+
+    console.print(table)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

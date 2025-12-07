e52421788482ee8f3376e8c80d5a2e982cf3c052
LKS <lingasamy.kandasamy@gmail.com>
2025-12-07 17:27:31 +0530

    similarStrings temp

--- test.py
+++ test.py
@@ -1,38 +1,41 @@
-def knightlOnAChessboard(n):
+def similarStrings(n, s, queries):
     # Write your code here
-    arr = []
-    for i in range(1, n):
-        results = []
-        for j in range(1, n):
-            r = []
-            #print(f"({i},{j})", end=" ")
-            # Implement BFS or DFS to find the shortest path
-            from collections import deque
-            visited = [[False]*n for _ in range(n)]
-            queue = deque()
-            queue.append((0, 0, 0))  # (x, y,
-            visited[0][0] = True
-            found = False
-            while queue:
-                x, y, dist = queue.popleft()
-                if x == n-1 and y == n-1:
-                    r.append(dist)
-                    found = True
-                    break
-                for dx, dy in [(i, j), (i, -j), (-i, j), (-i, -j), (j, i), (j, -i), (-j, i), (-j, -i)]:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                        visited[nx][ny] = True
-                        queue.append((nx, ny, dist + 1))
-            if found:
-                #print(r[0], end=" ")
-                results.append(r[0])
-            else:
-                #print(-1, end=" ")  
-                results.append(-1)
-        #print(results)
-        arr.append(results)    
-        #print()
-    return arr
+    c = []
+    for query in queries:
+        l = query[0]
+        r = query[1]
+        substr = s[l-1:r]
+        if len(substr) == 1:
+            c.append(len(s))
+            continue
+        #print(substr)
+        ss = [s[i:i+r-l+1] for i in range(len(s)) if i+r-l+1 <= len(s)]
+#        print(substr)
+#        print(ss)
+        count = 0
+        for sub in ss:
+            check = True
+            for i in range(0,len(substr)):
+                for j in range(i+1, len(substr)):
+#                    print(i,j)
+#                    print(substr[i], substr[j], sub[i], sub[j])
+                    if (substr[i] == substr[j] and sub[i] == sub[j]) or (substr[i] != substr[j] and sub[i] != sub[j]):
+                        continue
+#                        check = True
+                    else:
+                        check = False
+                        break
+            if check:
+                count += 1
+        c.append(count)
+        #print(c)
 
-print(knightlOnAChessboard(5))
+        #print(ss)
+    return c 
+
+n = 8
+s = "giggabaj"
+queries = [[1,1],[1,2],[1,3],[2,4]]
+#queries = [[2,4]]
+print(similarStrings(5, s, queries))
+

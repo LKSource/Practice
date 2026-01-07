3fbdd070d7d33e5dcfb52e9f8b937095478aaccc
LKS <lingasamy.kandasamy@gmail.com>
2026-01-07 11:48:46 +0530

    formatting

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+# from itertools import permutations
 
 def larrysArray(A):
     count = 0
@@ -7,9 +7,10 @@ def larrysArray(A):
             if A[i] > A[j]:
                 count += 1
     return "YES" if count % 2 == 0 else "NO"
-    
+
+
 A = [[3, 1, 2], [1, 3, 4, 2], [1, 2, 3, 5, 4], [1, 6, 5, 2, 4, 3]]
-#A = [[1, 6, 5, 2, 4, 3]]
+# A = [[1, 6, 5, 2, 4, 3]]
 
 for i in A:
     print(larrysArray(i))
